8518b1a54da17337041e2b709bb4e2521cd19fea
Md Abdul Matin <matinpcc@yahoo.com>
2023-04-10 22:59:10 +0200

    Updated functions and styles

--- To-do-list/src/Components/Todo.jsx
+++ To-do-list/src/Components/Todo.jsx
@@ -1,9 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Todo = () => {
+const Todo = (props) => {
+  const [input, setInput] = useState("");
+  const [isUpdate, setisUpdate] = useState(false);
+
+  const handleChange = (event) => {
+    setInput(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    props.editTodo(props.id, input);
+    setInput("");
+  };
+
+  const defaultTemplete = (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="checkbox"
+        id={props.id}
+        defaultChecked={props.completed}
+        onChange={() => props.toggleCompleted(props.id)}
+      />
+      {props.task}
+      <div className="buttonsDiv">
+        <button
+          className="editButton"
+          type="button"
+          onClick={() => setisUpdate(true)}
+        >
+          Edit
+        </button>
+        <button
+          className="deleteButton"
+          type="button"
+          onClick={() => props.deleteTodo(props.id)}
+        >
+          Delete
+        </button>
+      </div>
+    </form>
+  );
+
+  const editTemplete = (
+    <form onSubmit={handleSubmit}>
+      <label> Update todo: {props.task}</label>
+      <input type="text" id={props.id} onChange={handleChange} value={input} />
+      {props.task}
+      <div className="buttonsDiv">
+        <button
+          className="deleteButton"
+          type="button"
+          onClick={() => setisUpdate(false)}
+        >
+          Cancel
+        </button>
+        <button className="editButton" type="submit">
+          Save
+        </button>
+      </div>
+    </form>
+  );
+
   return (
-    <div className="container">
-      <h1>Todo List</h1>
+    <div>
+      <li>{isUpdate ? editTemplete : defaultTemplete}</li>
     </div>
   );
 };
--- To-do-list/src/Components/TodoList.jsx
+++ To-do-list/src/Components/TodoList.jsx
@@ -1,10 +1,93 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import {
+  addTodosDB,
+  deleteTodoDB,
+  fetchFromDB,
+  updateTodosDB,
+} from "../DB/operation";
 import Todo from "./Todo";
 
 const TodoList = () => {
+  const [input, setInput] = useState("");
+  const [todos, setTodos] = useState([]);
+  const handleChange = (event) => {
+    setInput(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    addTodo();
+    setInput("");
+  };
+
+  const toggleCompleted = (id) => {
+    toggled ? setToggle(false) : setToggle(true);
+    console.log("in editTodo", id);
+    const editedList = todos.map((item) => {
+      if (id === item.id) {
+        updateTodosDB(id, { ...item, completed: !item.completed });
+        return { ...item, completed: !item.completed };
+      }
+      return item;
+    });
+    setTodos(editedList);
+  };
+  const addTodo = () => {
+    const newTodo = {
+      task: input,
+      completed: false,
+    };
+    addTodosDB(newTodo);
+    setTodos([...todos, newTodo]);
+  };
+
+  const editTodo = (id, newTask) => {
+    const editedTodos = todos.map((item) => {
+      if (id == item.id) {
+        updateTodosDB(id, { ...item, task: newTask });
+        return { ...item, task: newTask };
+      }
+      return item;
+    });
+    setTodos(editedTodos);
+  };
+  const deleteTodo = (id) => {
+    const remainingTodos = todos.filter((item) => {
+      return id !== item.id;
+    });
+    deleteTodoDB(id);
+    setTodos(remainingTodos);
+  };
+
+  useEffect(() => {
+    fetchFromDB().then((newTodo) => {
+      setTodos(newTodo);
+    });
+  }, []);
+
   return (
-    <div>
-      <Todo />
+    <div className="container">
+      <h1>Todo List</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="">Add a todo: </label>
+        <input type="text" onChange={handleChange} value={input} />
+        <button type="submit">Add</button>
+      </form>
+      <ul>
+        {todos.map((item) => {
+          return (
+            <Todo
+              key={item.id}
+              id={item.id}
+              task={item.task}
+              completed={item.completed}
+              toggleCompleted={toggleCompleted}
+              editTodo={editTodo}
+              deleteTodo={deleteTodo}
+            />
+          );
+        })}
+      </ul>
     </div>
   );
 };
